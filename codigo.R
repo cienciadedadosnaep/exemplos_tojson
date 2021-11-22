e330927722ecb7705a3c02524eb35402a9c5320a
rpessoa1983 <robsonpessoa2007@gmail.com>
2021-11-21 21:31:58 -0300

    solução dataset source lado a lado echarts

--- codigo.R
+++ codigo.R
@@ -1,71 +1,53 @@
 library(jsonlite)
+library(rjson)
+library(RJSONIO)
 ano <- c('2012','2013','2014','2015')
 MatchaLatte<- c(41.1, 30.4, 65.1, 53.3)
 MilkTea<- c(86.5, 92.1, 85.7, 83.1)
 CheeseCocoa<-c(24.1, 67.2, 79.5, 86.4)
-df <- data.frame(ano, MatchaLatte, MilkTea, CheeseCocoa)
-View(df)
-jsonlite::toJSON(df,dataframe="columns")
 
-jsonlite::toJSON(df,dataframe="values")
 
+df <- data.frame(ano,MatchaLatte,MilkTea,CheeseCocoa)
 
 dft <- t(df)
-View(dft)
-jsonlite::toJSON(dft,dataframe="columns")
 
+names(dft)
 
+dft_n<- data.frame(as.character(row.names(dft)),dft)
+
+names(dft_n) <- c("X0","X1","X2","X3","X4")
+
+names(dft_n)
 library(dplyr)
+dft_n<- dft_n %>% mutate(across(c(2,3,4,5), as.numeric))
+
+    rownames(dft_n)<-NULL
+    
+    
+teste5<-  jsonlite::toJSON(dft_n,dataframe = "values")    
+teste5
+##########################################################    
+
+dft_nlanos <- dft_n[1,]
+names(dft_nlanos) <- NULL 
+dft_nlanos <- as.character(dft_nlanos)
 
 
-row.names(dft)
+dft_nl <-  dft_n[-c(1),]
 
 
-dft_a <- dft %>% mutate(V0=row.names())
 
-nomebar <- row.names(dft)
+teste6 <- list(dft_nlanos,dft_nl)
+    
+    
+teste7<- jsonlite::toJSON(teste6,dataframe = "values")    
 
-dft_a <- dft %>% mutate(V0 = nomebar)
 
-cbind(nomebar,dft)
+teste8 <- gsub('\\[\\[','[',teste7)
+teste9 <- gsub('\\]\\]\\]',']',teste8)
+teste9 
 
-dft_cb <- cbind(nomebar,dft)
-jsonlite::toJSON(dft_cb,dataframe="columns")
 
-matrix(c(ano,MatchaLatte,MilkTea,CheeseCocoa),ncol = 4)
 
-names(dft_cb) <- c('v0','v1','v2','v3','v4')
-names()
-
-names(dft_cb)
-
-dft_cb
-df$ano
-jsonlite::toJSON(df$ano)
-
-jsonlite::toJSON(df$ano,dataframe = "columns")
-
-jsonlite::toJSON(df$ano,dataframe = "values")
-
-dfl <- as.list(df)
-View(dfl)
-
-jsonlite::toJSON(dfl,dataframe = "columns")
-
-jsonlite::toJSON(dfl,dataframe = "values")
-
-jsonlite::toJSON(dfl,dataframe = "rows")
-
-jsonlite::toJSON(dfl,dataframe = "array")
-
-dfarray <- as.array(df)
-
-dfarray <- as.array(dfl)
-dfarray
-
-jsonlite::toJSON(dfarray,dataframe = "columns")
-
-jsonlite::toJSON(dfarray,dataframe = "values")
-
-jsonlite::toJSON(dfarray,dataframe = "rows")
+data_serie <- paste('[',teste9,']',sep = '')
 
